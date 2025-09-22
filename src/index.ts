0cac2b0bfc24a2ad7a9c061b89eb199a6db53f55
theridwanade <theridwanade@gmail.com>
2025-09-22 18:15:28 +0100

    Refactor URL handling logic to improve database interaction and error management

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 
-// const urlDatabase: Map<string, string> = new Map();
+const urlDatabase: Map<string, string> = new Map();
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
@@ -29,22 +29,27 @@ app.post("/shorten", async (req, res) => {
     }
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     const shortid = customAlphabet(alphabet, 6)();
-    // urlDatabase.set(shortid, url);
     await saveUrl({shortCode: shortid, url});
+    urlDatabase.set(shortid, url);
     res.json({ shortUrl: `${req.protocol}://${req.get("host")}/${shortid}` });
 })
 
 app.get("/:shortid", async (req, res) => {
-    const {shortid} = req.params;
-    // const originalUrl = urlDatabase.get(shortid);
-    const originalUrl = await getUrl(shortid);
-    if (!originalUrl) {
-        return res.status(404).json({error: "URL not found"});
-    }
-    res.redirect(originalUrl);
+    try {
+        const {shortid} = req.params;
+        const originalUrl = urlDatabase.get(shortid) || await getUrl(shortid);
 
+        if (!originalUrl) {
+            return res.status(404).json({error: "URL not found"});
+        }
+        urlDatabase.set(shortid, originalUrl);
+
+        res.redirect(originalUrl);
+    } catch (error) {
+        return res.status(500).json({error: "Internal Server Error"});
+    }
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on =http://localhost:${PORT}`);
 })
